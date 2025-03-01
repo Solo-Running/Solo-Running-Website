cc021256d3ca080fea414a0df5f104d4d9efbbbe
William Kim <wik002@ucsd.edu>
2025-02-28 18:14:09 -0800

    removed unused imports

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import {usePathname } from "next/navigation";
 import { useState } from 'react';
-import { IoIosArrowDown, IoIosArrowDropup, IoIosArrowUp } from 'react-icons/io';
+import { IoIosArrowDown} from 'react-icons/io';
 
 
 export default function NavBar() {
